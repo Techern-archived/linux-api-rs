b00386622e272f8b399bad4caadc97ce901f9858
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-07-18 19:16:43 +1000

    Initial Cargo project. Just testing Travis CI for now
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! This is a very early work-in-progress binding to various Linux Kernel APIs.
+//!
+//! It is not yet ready for use in your projects. Once version 0.1 or higher
+//! is released, you are welcome to start using it :)
+
+//No actual bindings yet, just checking Travis CI
+#[test]
+fn it_works() {
+}
