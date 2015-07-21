64fa0dd570cbc46565e7f4c5ff1ecf3f03b07a97
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-07-22 09:24:46 +1000

    linux-api-sys: Add geteuid(), getgid(), getegid()
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>

--- lib/linux-api-sys/src/lib.rs
+++ lib/linux-api-sys/src/lib.rs
@@ -3,7 +3,7 @@
 
 //First, let's use linux-api stuff
 extern crate linux_api;
-use linux_api::{pid_t, uid_t};
+use linux_api::{pid_t, uid_t, gid_t};
 
 //Okay, let's do this!
 extern "C" {
@@ -16,6 +16,15 @@ extern "C" {
     
     ///Returns the current user's ID
     pub fn getuid() -> uid_t;
+    
+    ///Returns the current effective user's ID
+    pub fn geteuid() -> uid_t;
+    
+    ///Returns the current user's (primary?) group ID
+    pub fn getgid() -> gid_t;
+    
+    ///Returns the current effective user's (primary?) group ID
+    pub fn getegid() -> gid_t;
 }
 
 #[cfg(test)]
@@ -28,6 +37,20 @@ mod test {
         unsafe { 
             assert!(getpid() >= 0); 
             assert!(getppid() >= 0); 
+        }
+    }
+    
+    #[test]
+    fn ensure_group_same() {
+        unsafe {
+            assert_eq!(getgid(), getegid());
+        }
+    }
+    
+    #[test]
+    fn ensure_user_same() {
+        unsafe {
+            assert_eq!(getuid(), geteuid());
         }
     }
 
