4b14ddc62e2a75867211a08fc390aed2d6d86228
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-07-23 00:57:21 +1000

    Implement exit() and exit_group()
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>

--- lib/linux-api-exit/src/lib.rs
+++ lib/linux-api-exit/src/lib.rs
@@ -1,3 +1,36 @@
-#[test]
-fn it_works() {
+//! Bindings to kernel/exit.c
+//!
+//! Currently only provides access to exit() and exit_group()
+
+#![cfg(target_os = "linux")]
+
+extern crate linux_api;
+
+use linux_api::{c_int};
+
+extern {
+
+    ///Exits the current process with the specified error code
+    ///
+    ///You should use either linux_api::EXIT_SUCCESS or linux_api::EXIT_FAILURE
+    pub fn exit(error_code: c_int);
+
+    ///Exits every process in the current process group with the specified error code
+    ///
+    ///You should use either linux_api::EXIT_SUCCESS or linux_api::EXIT_FAILURE
+    pub fn exit_group(error_code: c_int);
+
+}
+
+#[cfg(test)]
+mod test {
+
+    use linux_api::{EXIT_SUCCESS, EXIT_FAILURE};
+
+    #[test]
+    fn ensure_exit_values_exist() {
+        assert_eq!(0, EXIT_SUCCESS);
+        assert_eq!(1, EXIT_FAILURE);
+    }
+
 }
