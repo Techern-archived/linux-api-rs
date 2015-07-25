0c5b2c372d7eb21560a863d19d98f4f8dc96b489
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-07-25 20:57:37 +1000

    Add mode_t and key_t, along with associated __kernel versions
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>

--- src/posix_types.rs
+++ src/posix_types.rs
@@ -3,8 +3,8 @@
 //! If unsure, check include/uapi/asm-generic/posix_types.h **first**, then /arch/*/include/uapi/asm/posix_types.h
 //!
 //! For the best result, use http://lxr.free-electrons.com
-
-
+//!
+//! Due for a complete makeover for 0.1
 
 #[cfg(any(target_arch="sparc", target_arch="parisc"))]
 ///A type that defines microseconds. Since you're using either SPARC or (pa)RISC, it is a c_int
@@ -79,6 +79,44 @@ pub type __kernel_gid_t = ::c_uint;
          target_arch = "x86_64", 
          target_arch = "m32r"))]
 pub type __kernel_gid_t = ::c_ushort;
+
+#[cfg(not(any(target_arch = "cris",
+              target_arch = "parisc",
+              target_arch = "microblaze",
+              target_arch = "m68k", 
+              target_arch = "sh", 
+              target_arch = "arm", 
+              target_arch = "avr32", 
+              target_arch = "sparc", 
+              target_arch = "frv", 
+              target_arch = "blackfin", 
+              target_arch = "mn10300", 
+              target_arch = "x86", 
+              target_arch = "x86_64",
+              target_arch = "m32r")))]
+pub type __kernel_mode_t = ::c_uint;
+
+#[cfg(any(target_arch = "cris", 
+         target_arch = "parisc",
+         target_arch = "microblaze",
+         target_arch = "m68k", 
+         target_arch = "sh", 
+         target_arch = "arm", 
+         target_arch = "avr32", 
+         target_arch = "sparc", 
+         target_arch = "frv", 
+         target_arch = "blackfin", 
+         target_arch = "mn10300", 
+         target_arch = "x86", 
+         target_arch = "x86_64", 
+         target_arch = "m32r"))]
+pub type __kernel_mode_t = ::c_ushort;
+
+pub type __kernel_key_t = ::c_int;
+
+pub type key_t = __kernel_key_t;
+
+pub type mode_t = __kernel_mode_t;
 
 #[cfg(target_arch="x86")]
 ///A kernel type for long, set to c_longlong snice you're on x86
